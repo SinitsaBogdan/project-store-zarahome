23193f0028958cc588b42e17441e8041f293d5fd
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:37:17 +0300

    create : widgets component Footer

---
+++ frontend/src/widgets/Footer/components/Footer.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import '../style/Footer.css';
+
+import json from '../data/index.json';
+
+// UI elements
+import { LogoUI } from '../../../shared/ui';
+
+export default function Footer() {
+	return (
+		<footer className='Footer'>
+			<nav className='Footer__nav'>
+				<div className='Footer__section'>
+					<h2 className='Footer__title'>Магазин</h2>
+					<div className='Footer__row_wrapper'>
+						<ul className='Footer__list'>
+							{json[0].store.first.map((item, index) => {
+								return (
+									<li key={index} className='Footer__navItem'>
+										<a href={item.url} className='Footer__navLink'>
+											{item.title}
+										</a>
+									</li>
+								);
+							})}
+						</ul>
+						<ul className='Footer__list'>
+							{json[0].store.last.map((item, index) => {
+								return (
+									<li key={index} className='Footer__navItem'>
+										<a href={item.url} className='Footer__navLink'>
+											{item.title}
+										</a>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+				<div className='Footer__section'>
+					<h2 className='Footer__title'>Помощь</h2>
+					<ul className='Footer__list'>
+						{json[0].helpers.map((item, index) => {
+							return (
+								<li key={index} className='Footer__navItem'>
+									<a href={item.url} className='Footer__navLink'>
+										{item.title}
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div className='Footer__section'>
+					<h2 className='Footer__title'>Контакты</h2>
+					{json[0].contacts.map((item, index) => {
+						return (
+							<li key={index} className='Footer__navItem'>
+								<a href={item.url} className='Footer__navLink'>
+									{item.title}
+								</a>
+							</li>
+						);
+					})}
+				</div>
+			</nav>
+			<p className='Footer__descriptions'>
+				Содержание этого сайта защищено авторским правом и является собственностью ZARAHOME.
+				ZARAHOME стремится к доступности.
+			</p>
+			<LogoUI />
+			<p className='Footer__rights'>© 2023 ZARAHOME. Все права защищены.</p>
+		</footer>
+	);
+}
