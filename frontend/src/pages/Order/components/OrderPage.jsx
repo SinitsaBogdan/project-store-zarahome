b0bf01f4e6799bf54ea9d7fe02d27fbf161b7763
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:27:45 +0300

    create : pages component Order

---
+++ frontend/src/pages/Order/components/OrderPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/OrderPage.css';
+
+// widgets
+import Header from '../../../widgets/Header';
+import Footer from '../../../widgets/Footer';
+
+export default function OrderPage() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
