8f1145859e5f1da3c84dbfc377cbfb28dec6bdd6
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:23:34 +0300

    create : pages component Catalog

---
+++ frontend/src/pages/Catalog/components/CatalogPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/CatalogPage.css';
+
+// widgets
+import Header from '../../../widgets/Header';
+import Footer from '../../../widgets/Footer';
+
+export default function CatalogPage() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
