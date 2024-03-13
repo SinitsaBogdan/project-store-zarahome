0c77ce689a36cbf21598d037dc6db30defc2dfb6
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:22:43 +0300

    create : pages component Cart

---
+++ frontend/src/pages/Cart/components/CartPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/CartPage.css';
+
+// widgets
+import Header from '../../../widgets/Header';
+import Footer from '../../../widgets/Footer';
+
+export default function CartPage() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
