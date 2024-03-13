7641179c3e8c37ac413d5f98709d76b0c1d60793
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:28:40 +0300

    create : pages component Search

---
+++ frontend/src/pages/Search/components/SearchPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/SearchPage.css';
+
+// widgets
+import Header from '../../../widgets/Header';
+import Footer from '../../../widgets/Footer';
+
+export default function SearchPage() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
