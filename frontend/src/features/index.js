31f41d6f594ad3205c5c60569c3515616940cd83
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:15:14 +0300

    create : layer features

---
+++ frontend/src/features/index.js
@@ -0,0 +1,4 @@
+export { default as Cart } from './Cart';
+export { default as Navigation } from './Navigation';
+export { default as FormSelectColor } from './FormSelectColor';
+export { default as Pagination } from './Pagination';
