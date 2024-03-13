51113d174c64d27c96dce451188d164647402e1a
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:30:00 +0300

    create : layer shared

---
+++ frontend/src/shared/ui/index.js
@@ -0,0 +1,6 @@
+export { default as LogoUI } from './LogoUI';
+export { default as ProfileUI } from './ProfileUI';
+export { default as LikeLinkUI } from './LikeLinkUI';
+export { default as CartLinkUI } from './CartLinkUI';
+export { default as CartButtonUI } from './CartButtonUI';
+export { default as LikeButtonUI } from './LikeButtonUI';
