fa989586588b42f91d7114e6b3ce3a39471209f7
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-13 23:28:09 +0300

    create : pages component Product

---
+++ frontend/src/pages/Product/components/ProductPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/ProductPage.css';
+
+// widgets
+import Header from '../../../widgets/Header';
+import Footer from '../../../widgets/Footer';
+
+export default function ProductPage() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
