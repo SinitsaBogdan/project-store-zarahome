91f87716326daebf48cb0743aa41ee8791c59016
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-16 13:27:39 +0300

    Update index.js

--- frontend/src/features/index.js
+++ frontend/src/features/index.js
@@ -2,3 +2,5 @@ export { default as Cart } from './Cart';
 export { default as Navigation } from './Navigation';
 export { default as FormSelectColor } from './FormSelectColor';
 export { default as Pagination } from './Pagination';
+export { default as RatingStar } from './RatingStar';
+export { default as Reviews } from './Reviews';
