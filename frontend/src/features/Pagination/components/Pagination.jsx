02efbd0f348dc5c3fd5819ea8e5dc2a04d5bc5c5
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-16 13:27:50 +0300

    update : Pagination

--- frontend/src/features/Pagination/components/Pagination.jsx
+++ frontend/src/features/Pagination/components/Pagination.jsx
@@ -5,12 +5,14 @@ import '../style/Pagination.css';
 export default function Pagination({ coll }) {
 	const list = Array.from(Array(coll).keys());
 	return (
-		<ul class='Pagination'>
+		<ul className='Pagination'>
 			{list.map((index, n) => {
 				return (
 					<li
 						key={index}
-						class={`Paginations__item ${index === 0 && 'Paginations__active'}`}></li>
+						className={`Paginations__item ${
+							index === 0 && 'Paginations__active'
+						}`}></li>
 				);
 			})}
 		</ul>
