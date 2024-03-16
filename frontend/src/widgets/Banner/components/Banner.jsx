6523f042622e5a2dd6339863d0e3cfa197f84f7a
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-16 13:28:32 +0300

    update : Banner

--- frontend/src/widgets/Banner/components/Banner.jsx
+++ frontend/src/widgets/Banner/components/Banner.jsx
@@ -7,13 +7,13 @@ import '../style/Banner.css';
 export default function Banner({ data }) {
 	return (
 		<section className='Banner'>
-			<div class='Banner__image_wrapper'>
-				<img class='Banner__image' src={data.image} alt={data.alt} />
+			<div className='Banner__image_wrapper'>
+				<img className='Banner__image' src={data.image} alt={data.alt} />
 			</div>
-			<div class='Banner__content'>
-				<h2 class='Banner__title'>{data.title}</h2>
-				<p class='Banner__descriptions'>{data.descriptions}</p>
-				<Link class='Banner__link' to={data.link}>
+			<div className='Banner__content'>
+				<h2 className='Banner__title'>{data.title}</h2>
+				<p className='Banner__descriptions'>{data.descriptions}</p>
+				<Link className='Banner__link' to={data.link}>
 					{data.linkText}
 				</Link>
 			</div>
