5a01f143af5249a1701d0f040537cc60af94897c
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-16 13:29:01 +0300

    update : PartitionSection

--- frontend/src/widgets/PartitionSection/components/PartitionSection.jsx
+++ frontend/src/widgets/PartitionSection/components/PartitionSection.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import '../style/PartitionSection.css';
 
 // Data
-import Partitions from '../data/index.json';
+import dataPartitions from '../data/index.json';
 
 // Entities
 import { Partition } from '../../../entities';
@@ -13,24 +13,15 @@ import { Partition } from '../../../entities';
 export default function PartitionSection() {
 	return (
 		<section className='PartitionSection'>
-			<h2 class='PartitionSection__title'>Каталог товаров</h2>
-			<ul class='PartitionSection__list'>
-				<li class='PartitionSection__item'>
-					<Partition data />
-					<a class='Partition' href='#!'>
-						<div class='Partition__image_wrapper'>
-							<img
-								class='Partition__image'
-								src='../images/catalog/item_1.png'
-								alt=''
-							/>
-						</div>
-						<div class='Partition__wrapper'>
-							<h2 class='Partition__title'>Спальня</h2>
-							<p class='Partition__text'>Для комфортного сна и отдыха</p>
-						</div>
-					</a>
-				</li>
+			<h2 className='PartitionSection__title'>Каталог товаров</h2>
+			<ul className='PartitionSection__list'>
+				{dataPartitions.map((item) => {
+					return (
+						<li key={item.id} className='PartitionSection__item'>
+							<Partition {...item} />
+						</li>
+					);
+				})}
 			</ul>
 		</section>
 	);
