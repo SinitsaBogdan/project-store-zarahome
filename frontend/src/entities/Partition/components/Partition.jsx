20928d03992a61109aaaf3d5ffa53c5a6d83f4ab
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-16 13:27:27 +0300

    update : Partition

--- frontend/src/entities/Partition/components/Partition.jsx
+++ frontend/src/entities/Partition/components/Partition.jsx
@@ -7,9 +7,12 @@ import '../style/Partition.css';
 export default function Partition(props) {
 	return (
 		<a href={props.href} className='Partition'>
-			<div class='Partition__content'>
-				<h2 class='Partition__title'>Спальня</h2>
-				<p class='Partition__text'>Для комфортного сна и отдыха</p>
+			<div className='Partition__img_wrapper'>
+				<img src={props.img} alt={props.alt} className='Partition__img' />
+			</div>
+			<div className='Partition__content'>
+				<h2 className='Partition__title'>{props.title}</h2>
+				<p className='Partition__text'>{props.description}</p>
 			</div>
 		</a>
 	);
