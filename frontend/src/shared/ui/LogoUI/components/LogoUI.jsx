c8d4e8ba4f4e9542905723706297159ecc413e75
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-17 23:39:58 +0300

    Update LogoUI.jsx

--- frontend/src/shared/ui/LogoUI/components/LogoUI.jsx
+++ frontend/src/shared/ui/LogoUI/components/LogoUI.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import { ReactComponent as LogoSVG } from '../../../assets/icon/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Logo({ link = '/' }) {
 	return (
-		<a href={link}>
-			<LogoSVG />
-		</a>
+		<Link className='Logo' to={link}>
+			<LogoSVG className='Logo__svg' />
+		</Link>
 	);
 }
