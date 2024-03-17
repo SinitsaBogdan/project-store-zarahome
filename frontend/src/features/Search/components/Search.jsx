ddcd1087d0e4e170f6ece4596a0ec09727995a99
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-17 23:38:49 +0300

    create : features Search

---
+++ frontend/src/features/Search/components/Search.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// Data
+import Products from '../../../entities/Product/data/index.json';
+
+import '../style/Search.css';
+import ProductSmall from '../../../entities/Product/components/ProductSmall';
+
+export default function Search({ value = 'кожан' }) {
+	return (
+		<article className='Search'>
+			<input
+				className='Search__input'
+				type='text'
+				placeholder='Что хотите найти?'
+				value='кожан'
+			/>
+			<ul className='Search__list'>
+				{Products.map((item, index) => {
+					console.log(item);
+					if (index <= 3) {
+						return (
+							<li key={index} className='Search__item'>
+								<ProductSmall {...item} />
+							</li>
+						);
+					} else {
+						return null;
+					}
+				})}
+			</ul>
+		</article>
+	);
+}
