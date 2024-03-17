05744b091f7a45adfc06e08e7eaef995e214ea5f
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-17 23:39:14 +0300

    Update CatalogPage.jsx

--- frontend/src/pages/Catalog/components/CatalogPage.jsx
+++ frontend/src/pages/Catalog/components/CatalogPage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import '../style/CatalogPage.css';
 
 // widgets
-import Header from '../../../widgets/Header';
-import Footer from '../../../widgets/Footer';
+import { Header, Footer, Catalog } from '../../../widgets';
 
 export default function CatalogPage() {
 	return (
 		<>
 			<Header />
+			<Catalog sort='bathroom' />
 			<Footer />
 		</>
 	);
