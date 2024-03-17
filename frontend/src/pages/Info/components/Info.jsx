3bca40bf99eb0b9f7c0ade3b0687e83c0abe67ee
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-17 23:39:32 +0300

    create : pages Info

---
+++ frontend/src/pages/Info/components/Info.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/Info.css';
+
+// widgets
+import { Header, Footer } from '../../../widgets';
+
+export default function Info() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
