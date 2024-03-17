523b2c8bc6ea6a538e4b07db69471ee82e7810c8
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-17 23:37:13 +0300

    Update index.js

--- frontend/src/entities/index.js
+++ frontend/src/entities/index.js
@@ -1,4 +1,5 @@
-export { default as Product } from './Product';
+export { default as Product } from './Product/components/Product';
+export { default as ProductSmall } from './Product/components/ProductSmall';
 export { default as Partition } from './Partition';
 export { default as Question } from './Question';
 export { default as Social } from './Social';
