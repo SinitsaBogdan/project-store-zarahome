c5fc8e2c1a9147b0cae040a0d81aeaf02a2b3e6d
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-26 00:45:15 +0300

    Update Social.jsx

--- frontend/src/entities/Social/components/Social.jsx
+++ frontend/src/entities/Social/components/Social.jsx
@@ -4,10 +4,10 @@ import '../style/Social.css';
 
 export default function Social({ href, img, alt, title, description }) {
 	return (
-		<a class='Social' href={href}>
-			<img class='Social__icon' src={img} alt={alt} />
-			<h2 class='Social__title'>{title}</h2>
-			<p class='Social__descriptions'>{description}</p>
+		<a className='Social' href={href}>
+			<img className='Social__icon' src={img} alt={alt} />
+			<h2 className='Social__title'>{title}</h2>
+			<p className='Social__descriptions'>{description}</p>
 		</a>
 	);
 }
