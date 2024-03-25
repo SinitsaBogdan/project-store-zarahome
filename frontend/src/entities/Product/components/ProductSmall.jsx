ad7e8a7b04b519d2fe97425939dc81c646d8e218
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-03-26 00:45:09 +0300

    Update ProductSmall.jsx

--- frontend/src/entities/Product/components/ProductSmall.jsx
+++ frontend/src/entities/Product/components/ProductSmall.jsx
@@ -6,13 +6,13 @@ import '../style/ProductSmall.css';
 
 export default function ProductSmall({ href, img, alt, title, description, price }) {
 	return (
-		<article class='ProductSmall'>
-			<a href={href} class='ProductSmall__link'>
-				<img class='ProductSmall__image' src={img} alt={alt} />
-				<div class='ProductSmall__content'>
-					<h2 class='ProductSmall__title'>{title}</h2>
-					<p class='ProductSmall__descriptions'>{description}</p>
-					<p class='ProductSmall__price'>
+		<article className='ProductSmall'>
+			<a href={href} className='ProductSmall__link'>
+				<img className='ProductSmall__image' src={img} alt={alt} />
+				<div className='ProductSmall__content'>
+					<h2 className='ProductSmall__title'>{title}</h2>
+					<p className='ProductSmall__descriptions'>{description}</p>
+					<p className='ProductSmall__price'>
 						{price} <span>₽</span>
 					</p>
 				</div>
